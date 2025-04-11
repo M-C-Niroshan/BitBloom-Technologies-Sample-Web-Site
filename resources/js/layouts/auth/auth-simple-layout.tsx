fae644597c0b32eabe3f19ba0cfc89b9348c1d9f
ChamikaNiroshan <M.ChamikaNiroshan@gmail.com>
2025-04-11 10:16:01 +0530

    Add header and footer to signup/ login
    Add header and footer to signup/ login

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,7 +1,8 @@
 import AppLogoIcon from '@/components/app-logo-icon';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
-
+import { Header } from '@/components/main-header';
+import { MainFooter } from '@/components/main-footer';
 interface AuthLayoutProps {
     name?: string;
     title?: string;
@@ -10,14 +11,15 @@ interface AuthLayoutProps {
 
 export default function AuthSimpleLayout({ children, title, description }: PropsWithChildren<AuthLayoutProps>) {
     return (
-        <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div className="w-full max-w-sm">
+        <div className="bg-background flex  flex-col items-center justify-center gap-6 ">
+            <Header />
+            <div className="w-full max-w-sm pt-20 pb-20">
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
+                            {/*<div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
                                 <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
-                            </div>
+                            </div>*/}
                             <span className="sr-only">{title}</span>
                         </Link>
 
@@ -29,6 +31,7 @@ export default function AuthSimpleLayout({ children, title, description }: Props
                     {children}
                 </div>
             </div>
+            <MainFooter />
         </div>
     );
 }
--- resources/js/pages/admin/customize-home/customize-home-slider.tsx
+++ resources/js/pages/admin/customize-home/customize-home-slider.tsx
@@ -1,10 +1,21 @@
 import AdminLayout from '@/layouts/admin/admin-layout';
+import SliderArea from '@/components/frontend/home-page/slider-area';
 
 export default function Customizeslider() {
     return (
         <div>
             <AdminLayout title="">
-                customize-Customizeslider
+                <div className="flex flex-col columns-2 gap-4 w-full border">
+                    <SliderArea admin_mode={true} />
+                </div>
+                <p className="text-2xl font-bold mt-4 mb-4 pt-3">
+                    Customize Home Page Slider
+                </p>
+                <div className="flex items-center justify-between">
+
+                    
+                </div>
+                <div className="flex items-center justify-between">dfg</div>
             </AdminLayout>
         </div>
     )
